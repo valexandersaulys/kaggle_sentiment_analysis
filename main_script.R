55acc295ee94a532f95c19ad4df5f555f8a2f784
Vincent Alexander Saulys <valexandersaulys@gmail.com>
2014-11-29 00:39:31 -0500

    now using RTextTools

--- main_script.R
+++ main_script.R
@@ -5,44 +5,29 @@
 
 rm(list = ls())  # clear workspace
 
-train <- read.delim(file="train.tsv", stringsAsFactors=FALSE)
+set <- read.delim(file="train.tsv", stringsAsFactors=FALSE)
 #test <- read.delim(file="test.tsv", stringsAsFactors=FALSE)
 
-#source('makeshift_funcs.R') # For my own functions, wasn't working so its commented out
-library(caret)  # For model training
+library(caret)  # For model training & other useful functions
 library(tm) # For Text Mining
 library(SnowballC) # For certain advanced Text Mining
 library(plyr)
+library(class) # To load k-Nearest Neighbour Classification (kNN)
+library(RTextTools)
 
+# We have 156,060 entries, so we split to 3/4 for training and 1/4 for validation
+len <- (3/4) * length(set[,1])
 
-clean_up <- function(df) {
-  library(tm)
-  corp <- Corpus(DataframeSource(df))
-  
-  
-  corp <- tm_map(corp, content_transformer(tolower))
-  corp <- tm_map(corp, removePunctuation)
-  corp <- tm_map(corp, removeNumbers)
-  corp <- tm_map(corp, removeWords, stopwords("english"))
-  corp <- tm_map(corp, stemDocument)
-  corp <- tm_map(corp, stripWhitespace)
-  
-  return(corp)
-}
+dm <- create_matrix(set$Phrase, removeStopwords=FALSE)
+container <- create_container(matrix=dm,  # This will take a long time to compute
+                              labels=set$Sentiment, 
+                              trainSize=1:(len-1),
+                              testSize=len:length(set[,1]), 
+                              virgin=FALSE  # Does the data have corresponding labels?, FALSE if it does
+                              )
 
+SVM <- train_model(container,'SVM')
+# returns a data.frame of predicted codes and probabilities for the specificied algorithm
+SVM_CLASSIFY <- classify_model(container,SVM)  
 
-# Make train's Sentiment rating into factor of five levels from 0 to 4
-train$Sentiment <- as.factor(train$Sentiment)
-
-# Splitting up the data set depending on the different sentiment ratings
-# I'm theorizing a Kmeans cluster will be found 
-# Whichever a given text is will be determined by the closest kmeans
-zero <- train[train$Sentiment==0,]
-one <- train[train$Sentiment==1,]
-two <- train[train$Sentiment==2,]
-three <- train[train$Sentiment==3,]
-four <- train[train$Sentiment==4,]
-
-myframes <- list(zero, one, two, three, four)
-
-corpi_train <- lapply(myframes, clean_up) 
+analytics <- create_analytic(container,SVM_CLASSIFY)
